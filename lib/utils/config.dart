6b6a638c9105aa5251d3596842bb4315b5bf7ba6
Alexandre Allain <alexandre.allain495@gmail.com>
2025-05-09 16:40:24 +0200

    killian

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   // URL de l'API utilisée en mode debug et en production
-  static const String baseUrl = 'http://ladetentedecamoel-web-7uucjr-41b780-192-168-157-50.traefik.me/api';
+  static const String baseUrl = 'http://ladetentedecamoel-web-7uucjr-41b780-192-168-147-50.traefik.me/api';
   static const String ordersEndpoint = '$baseUrl/addCommande';
   
   // Configuration pour autoriser le trafic non sécurisé (HTTP)
